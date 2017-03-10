2497568bdc71a7f47d8e76ae6edb8c4b76696cd3
Punkbooster <ugliar.arsen@gmail.com>
2017-03-10 18:27:31 +0100

    Add onChange event listener

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,22 @@
-import React from 'react'
+// that {component} is the same this as const Component = React.Component
+import React, { Component } from 'react'
 
 // this is called a function component, becaus it's literaly just a funcion
-const SearchBar = () => {
-  return <input />
+// function components should be used when you need a dump components (components without any logic, just a plain html)
+// const SearchBar = () => {
+//   return <input />
+// }
+
+// class based component, it's just a plain javascript object
+// extends React.Component extends our searchBar class with react component methods
+class SearchBar extends Component {
+  render() {
+    return <input onChange={this.onInputChange} />
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
 }
 
 export default SearchBar
