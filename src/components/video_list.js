19431a08978670708a6dbbfbcbf02a2aa665e0b4
Punkbooster <ugliar.arsen@gmail.com>
2017-03-17 11:55:25 +0100

    Add video list item component & iterate on the videos

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import VideoListItem from './video_list_item'
 
 // example of function based component with props
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} vide={video} />
+  })
+
   return (
     <ul className='col-md-4 list-group'>
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 }
