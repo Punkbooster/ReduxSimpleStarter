90512abd1c5c8f96106f44fbbb8d375c82bcdef9
Punkbooster <ugliar.arsen@gmail.com>
2017-04-05 16:43:22 +0200

    Add selected video to state and change pass it to child components

--- src/index.js
+++ src/index.js
@@ -17,11 +17,18 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       // because of the ES6 { videos: videos} can be written like so:
-      this.setState({ videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
+
     })
   }
 
@@ -29,8 +36,12 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          // also can be written as: (selectedVideo) => this.setState({selectedVideo})
+          onVideoSelect={(video) => this.setState({selectedVideo: video})}
+          videos={this.state.videos}
+        />
       </div>
     )
   }
