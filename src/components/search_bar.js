926b76f739227a2ddeaa1948e5b75b14fea58172
Punkbooster <ugliar.arsen@gmail.com>
2017-04-07 12:31:23 +0200

    Add some styles

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -25,7 +25,7 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className='search-bar'>
         <input
           value={this.state.term}
           onChange={this.onInputChange.bind(this)}
